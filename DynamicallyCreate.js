08859152a186120372fa5a17a48c12a87616b623
David M. Golembiowski <dmgolembiowski@gmail.com>
2019-04-23 20:33:13 -0400

    Create DynamicallyCreate.js

---
+++ DynamicallyCreate.js
@@ -0,0 +1,8 @@
+<script>
+function myFunction(text) {
+    var h = document.createElement("H1");
+    var t = document.createTextNode(text);
+    h.appendChild(t);
+    document.body.appendChild(h);
+}
+</script>
